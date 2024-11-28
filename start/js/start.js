6748d5029a7cfcf146616800b5e43e7fc6470191
minji0626 <jo010163@naver.com>
2024-11-28 16:51:11 +0900

    qna select

--- start/js/start.js
+++ start/js/start.js
@@ -1,6 +1,32 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
+function addAnswer(answerText, qIdx){
+    var a = document.querySelector('.answerBox');
+    var answer = document.createElement('button');
+    answer.classList.add('answerList');
+    a.appendChild(answer);
+    answer.innerHTML = answerText;
+
+    answer.addEventListener("click", function(){
+        var childern = document.querySelectorAll('.answerList');
+        for(let i = 0; i < childern.length; i++){
+            childern[i].disabled = true;
+            childern[i].style.display = 'none';
+        }
+        goNext(++qIdx);
+    },false);
+}
+
+
+function goNext(qIdx){
+    var q = document.querySelector('.qBox');
+    q.innerHTML = qnaList[qIdx].q;
+    for(let i in qnaList[qIdx].a){
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+    }
+}
+
 function begin(){
     main.style.WebkitAnimation = "fadeOut 1s";
     main.style.animation = "fadeOut 1s";
@@ -12,5 +38,7 @@ function begin(){
             main.style.display = "none";
             qna.style.display = "block";
         }, 450);
+        let qIdx = 0;
+        goNext(qIdx);
     }, 450);
 }
